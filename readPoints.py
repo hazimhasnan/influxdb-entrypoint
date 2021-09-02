a0a6be5bc0c4511d329a602e61db337f2a32e6c2
HazimHasnan <hazim.hasnan@xpress.ai>
2021-09-02 21:46:07 +0800

    Fixed the flux query in

--- readPoints.py
+++ readPoints.py
@@ -1,15 +1,26 @@
 from datetime import datetime
+from types import TracebackType
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate a Token from the "Tokens Tab" in the UI
-token = "Rvkv9YM6U4S9rcY-8gijLBRAQfILr2ixkH1JXE-ozM2uw-A5Nk9wI3VHcupXNAJ-9wd4qa9guCKUN59athWp5w=="
+token = "eehoW7NSFeObcVPPN9evABWtlJG9jBjgLUs_arDuGOqJPGsYAHJ6hN2XTGakbhPhFsxecCPa-NhLytdgqvvU4w=="
 org = "skymind"
 bucket = "python-bucket"
 
 client = InfluxDBClient(url="http://localhost:8086", token=token)
 
 # Create a query and pass the query to the api
-query = f'from(bucket: \\"{bucket}\\") |> range(start: -1h)'
+query = f'from(bucket: "{bucket}") |> range(start: -1h) \
+        |> filter(fn:(r) => r._measurement == "memory")'
+        
 tables = client.query_api().query(query, org=org)
+
+for i, table in enumerate(tables):
+    print(f"Table {i}")
+    for record in table:
+        # print(record)
+        print(record.get_field())
+        print(record.get_value())
+
